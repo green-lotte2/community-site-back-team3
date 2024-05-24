4fcbd9464b8db11073d69c30debd582f99c83c4a
sun-gwang <kkj80911@gmail.com>
2024-05-24 12:49:45 +0900

    cicdtest4

--- src/test/java/kr/co/orangenode/CommunitySiteBackTeam3ApplicationTests.java
+++ src/test/java/kr/co/orangenode/CommunitySiteBackTeam3ApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.orangenode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CommunitySiteBackTeam3ApplicationTests {
 
     @Test
