3ccaded9fd7533050e1703d9d9d40ca524d522cd
그럼컴퓨터 롯데2차 과정 <167052369+green-lotte2@users.noreply.github.com>
2024-05-29 17:14:05 +0900

    글 수정 뷰페이지 작업 중

--- src/main/java/kr/co/orangenode/repository/ArticleRepository.java
+++ src/main/java/kr/co/orangenode/repository/ArticleRepository.java
@@ -5,8 +5,10 @@ import kr.co.orangenode.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    public List<Article> findAllByOrderByAnoDesc();
 }
