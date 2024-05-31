751af2d457740203a0b4d47ca3ca82918ab42539
linma21 <154877422+linma21@users.noreply.github.com>
2024-05-31 10:00:33 +0900

    repo

--- src/main/java/kr/co/orangenode/repository/Custom/ChatRoomRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package kr.co.orangenode.repository.custom;
-
-import kr.co.orangenode.entity.chat.ChatRoom;
-
-import java.util.List;
-
-public interface ChatRoomRepositoryCustom {
-    List<ChatRoom> findChatRoomsByUid(String uid);
-}
--- src/main/java/kr/co/orangenode/repository/Custom/ProjectRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package kr.co.orangenode.repository.custom;
-
-import com.querydsl.core.Tuple;
-
-import java.util.List;
-
-public interface ProjectRepositoryCustom {
-    public List<Tuple> selectKanban(int proNo);
-}
